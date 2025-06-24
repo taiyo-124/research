12686c8337ac5f10c7a8be4bda2e2bcb75a05818
Taiyoh Kawashima <kawashima@icn.ist.hokudai.ac.jp>
2025-06-24 16:35:24 +0900

    効率化

--- RasPi/analyze/DataAnalyze.py
+++ RasPi/analyze/DataAnalyze.py
@@ -14,47 +14,52 @@ import matplotlib.dates as mdates
 
 
 # データ読み込み(~/Data/以下指定) "file_間隔"で指定
-#
-file_1MIN = 'LoRa/1MINDeepSleepLoRa.csv'
-df_1MIN = pd.read_csv(f'/home/kawashima/Data/{file_1MIN}', index_col=0, skiprows=1, names=["temperature", "humidity", "pressure", "voltage", "RSSI"])
-print(df_1MIN)
-print(len(df_1MIN))
+df_list = []
 
 file_1SEC = 'LoRa/1SECDeepSleepLoRa.csv'
 df_1SEC = pd.read_csv(f'/home/kawashima/Data/{file_1SEC}', index_col=0, skiprows=1, names=["temperature", "humidity", "pressure", "voltage", "RSSI"])
+df_list.append(df_1SEC)
 print(df_1SEC)
 print(len(df_1SEC))
 
+file_2SEC = 'LoRa/2SECDeepSleepLoRa.csv'
+df_2SEC = pd.read_csv(f'/home/kawashima/Data/{file_2SEC}', index_col=0, skiprows=1, names=["temperature", "humidity", "pressure", "voltage", "RSSI"])
+df_list.append(df_2SEC)
 
-""" 以下, RasPi側でデータを取得したときに実行(df.index: %H:%M:%S) """
-# 電圧値を3つ単位で平均化
-group_size_1MIN = 5
-group_size_1SEC = 5
+file_5SEC = 'LoRa/5SECDeepSleepLoRa.csv'
+df_5SEC = pd.read_csv(f'/home/kawashima/Data/{file_5SEC}', index_col=0, skiprows=1, names=["temperature", "humidity", "pressure", "voltage", "RSSI"])
+df_list.append(df_5SEC)
+
+file_1MIN = 'LoRa/1MINDeepSleepLoRa.csv'
+df_1MIN = pd.read_csv(f'/home/kawashima/Data/{file_1MIN}', index_col=0, skiprows=1, names=["temperature", "humidity", "pressure", "voltage", "RSSI"])
 
 # RSSIで条件をつけてデータを取り出す
 df_1MIN = df_1MIN[df_1MIN['RSSI'] > -100]
 
-# 経過時間に変換(Timedelta型)
-df_1MIN.index = pd.to_datetime(df_1MIN.index)
-elapsed_s_1MIN = (df_1MIN.index - df_1MIN.index[0])
-df_1MIN.index = pd.to_timedelta(elapsed_s_1MIN, unit='s')
-# Timedelta型を時間単位に
-df_1MIN.index = df_1MIN.index.total_seconds() / 60 / 60 
+df_list.append(df_1MIN)
 print(df_1MIN)
 
-df_1SEC.index = pd.to_datetime(df_1SEC.index)
-elapsed_s_1SEC = (df_1SEC.index - df_1SEC.index[0])
-df_1SEC.index = pd.to_timedelta(elapsed_s_1SEC, unit='s')
-df_1SEC.index = df_1SEC.index.total_seconds() / 60 / 60
-print(df_1SEC)
+print(df_list)
 
-# 'voltage'の平均化
-df_1MIN['voltage'] = df_1MIN.groupby(np.arange(len(df_1MIN)) // (group_size_1MIN))['voltage'].transform('mean')
-print(df_1MIN)
+""" 以下, RasPi側でデータを取得したときに実行(df.index: %H:%M:%S) """
+# 電圧値を3つ単位で平均化
+group_size = 5
+group_size_1MIN = 5
+group_size_1SEC = 5
+group_size_2SEC = 5
+group_size_5SEC = 5
 
-# 'voltage'の平均化
-df_1SEC['voltage'] = df_1SEC.groupby(np.arange(len(df_1SEC)) // group_size_1SEC)['voltage'].transform('mean')
-print(df_1SEC)
+group_size_list = [5, 5, 5, 5]
+
+for df in df_list:
+    df.index = pd.to_datetime(df.index)
+    elapsed_s_1SEC = (df.index - df.index[0])
+    df.index = pd.to_timedelta(elapsed_s_1SEC, unit='s')
+    df.index = df.index.total_seconds() / 60 / 60
+    print(df)
+
+    # 'voltage'の平均化
+    df['voltage'] = df.groupby(np.arange(len(df)) // (group_size))['voltage'].transform('mean')
 
 
 
@@ -76,8 +81,10 @@ print(df_1SEC)
 # 経過時間と電圧のグラフを表示
 
 plt.figure(figsize=(10, 5))
-plt.plot(df_1MIN.index, df_1MIN['voltage'], label="DeepSleep 1 Min")
-plt.plot(df_1SEC.index, df_1SEC['voltage'], label="DeepSleep 1 Sec")
+plt.plot(df_1MIN.index, df_1MIN['voltage'], label="1 Min")
+plt.plot(df_1SEC.index, df_1SEC['voltage'], label="1 Sec")
+plt.plot(df_2SEC.index, df_2SEC['voltage'], label="2 Sec")
+plt.plot(df_5SEC.index, df_5SEC['voltage'], label='5 Sec')
 
 
 plt.xlabel("Elapsed Time [hour]")
